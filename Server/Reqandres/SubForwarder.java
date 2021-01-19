aff725fac80e228fd06369fcb75f8bf1ca6d4117
Reza Fatahi <rezagames78@gmail.com>
2021-01-19 14:23:25 +0330

    Fixing forwarding algorithm

--- Server/Reqandres/SubForwarder.java
+++ Server/Reqandres/SubForwarder.java
@@ -4,18 +4,13 @@ import Server.Utils.Logger;
 import java.net.Socket;
 import java.io.*;
 
-public class SubForwarder extends Thread {
-
-    private Socket s;
-    private final File temp;
-    private final DataOutputStream clientOut;
+public class SubForwarder{
 
     public SubForwarder(String[] address, File tempFile,DataOutputStream out,String ip,String Host){
-        this.temp = tempFile;
-        this.clientOut = out;
         try{
-            s = new Socket(address[0], Integer.parseInt(address[1]));
-            this.start();
+            Socket s = new Socket(address[0], Integer.parseInt(address[1]));
+            new Piper(tempFile,s.getOutputStream(),s);
+            new Piper(s.getInputStream(),out,s);
             Logger.glog("Forwarding request for " + Host + " from " + ip + " to " + address[0] + ":" + address[1],Host);
         }catch(Exception ex){
             String t = "";
@@ -27,25 +22,48 @@ public class SubForwarder extends Thread {
         }
     }
 
-    @Override
-    public void run(){
-        try{
-            FileInputStream fl = new FileInputStream(temp);
-            OutputStream serverOut = s.getOutputStream();
-            InputStream serverIn = s.getInputStream();
-            fl.transferTo(serverOut);
-            serverOut.flush();
-            temp.delete();
-            serverIn.transferTo(clientOut);
-            clientOut.flush();
-            s.close();
-        }catch(Exception ex){
-            String t = "";
-            for (StackTraceElement a : ex.getStackTrace()){
-                t += a.toString() + " ;; ";
-            }
-            t += ex.toString();
-            Logger.ilog(t);
+    private class Piper extends Thread {
+
+        private File fl;
+        private final OutputStream out;
+        private InputStream in;
+        private final Socket connection;
+
+        public Piper (File cachedFile, OutputStream os, Socket s){
+            this.fl = cachedFile;
+            this.out = os;
+            this.connection = s;
+            this.start();
         }
+
+        public Piper (InputStream is, OutputStream os, Socket s){
+            this.in = is;
+            this.out = os;
+            this.connection = s;
+            this.start();
+        }
+
+        @Override
+        public void run(){
+            try{
+                if (fl != null){
+                    FileInputStream fis = new FileInputStream(fl);
+                    fis.transferTo(out);
+                }else
+                    in.transferTo(out);
+                out.flush();
+            }catch(Exception ex){
+                try {
+                    connection.close();
+                }catch (Exception ignored){}
+                String t = "";
+                for (StackTraceElement a : ex.getStackTrace()){
+                    t += a.toString() + " ;; ";
+                }
+                t += ex.toString();
+                Logger.ilog(t);
+            }
+        }
+
     }
 }
