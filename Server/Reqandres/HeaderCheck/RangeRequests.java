c41b594fd258b79e2547b198f7c8c110531ed827
SakoDroid <rezafattahi85@hotmail.com>
2021-04-04 12:33:33 +0430

    Adding If-Range header support

--- Server/Reqandres/HeaderCheck/RangeRequests.java
+++ Server/Reqandres/HeaderCheck/RangeRequests.java
@@ -1,15 +1,23 @@
 package Server.Reqandres.HeaderCheck;
 
 import Server.Reqandres.Request.Request;
+import Server.Utils.Configs.Configs;
+import Server.Utils.HeaderRelatedTools.HashComputer;
+import Server.Utils.HeaderRelatedTools.LMGenerator;
+import java.io.File;
 
 public class RangeRequests {
 
     private final Request req;
+    private boolean isIfRangeValid = true;
 
     public RangeRequests(Request req){
         this.req = req;
+        String ifRangeHeader = req.getHeaders().get("If-Range");
+        if (ifRangeHeader != null)
+            this.processIfRange(ifRangeHeader);
         String rangeHeader = req.getHeaders().get("Range");
-        if (rangeHeader != null)
+        if (rangeHeader != null && isIfRangeValid)
             this.processRange(rangeHeader);
     }
 
@@ -25,6 +33,17 @@ public class RangeRequests {
                     req.addRange(new long[]{Long.parseLong(vals[0]),Long.parseLong(vals[1])});
                 }
             }
+        }
+    }
+
+    private void processIfRange(String header){
+        header = header.replace("w/","");
+        String hash = new HashComputer(new File(Configs.getMainDir(req.getHost()) + req.getPath())).computeHash();
+        if (header.equals(hash))
+            isIfRangeValid = true;
+        else{
+            String lm = new LMGenerator(new File(Configs.getMainDir(req.getHost()) + req.getPath())).generate();
+            isIfRangeValid = header.equals(lm);
         }
     }
 }
